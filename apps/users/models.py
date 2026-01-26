b26195da0480c494ebffe055cb928cde673abbe9
SubhabrataBarik <subhabratabarik7@gmail.com>
2026-01-26 15:13:33 +0530

    done with task backend all issues fixeds

--- apps/users/models.py
+++ apps/users/models.py
@@ -42,6 +42,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_login = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    timezone = models.CharField(
+    max_length=50,
+    default="UTC"
+    )
+
 
     objects = UserManager()
 
@@ -50,36 +55,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-
-
-
-# Create your models here.
-# models.py
-
-# import django base user model tools
-
-# create CustomUser model instead of default Django User
-
-# fields needed:
-# - id (UUID primary key)
-# - email (unique, used as login)
-# - password (hashed automatically by Django)
-# - is_active (true/false)
-# - is_staff (admin access)
-# - is_superuser
-# - created_at
-# - updated_at
-
-# IMPORTANT:
-# - username is NOT needed
-# - email will be used as USERNAME_FIELD
-
-# use AbstractBaseUser + PermissionsMixin
-
-# attach custom UserManager (defined in managers.py)
-
-# set:
-# USERNAME_FIELD = "email"
-# REQUIRED_FIELDS = []
-
-# create __str__ to return email
