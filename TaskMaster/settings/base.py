068e0a87d94ed5a7a4710e78e4e5534dbe8558fa
SubhabrataBarik <subhabratabarik7@gmail.com>
2026-01-31 13:45:11 +0530

    frontend ready for vercel DOne

--- TaskMaster/settings/base.py
+++ TaskMaster/settings/base.py
@@ -18,7 +18,8 @@ import environ
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 env = environ.Env(DEBUG=(bool, False))
-env.read_env(BASE_DIR / ".env")
+if os.path.exists(BASE_DIR / ".env"):
+    env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
--- TaskMaster/wsgi.py
+++ TaskMaster/wsgi.py
@@ -11,11 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault(
-#     "DJANGO_SETTINGS_MODULE",
-#     os.getenv("DJANGO_SETTINGS_MODULE", "TaskMaster.settings.prod")
-# )
-
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "TaskMaster.settings.prod"
