7009e9eca6454f88d6ef428395632fe554ff6463
SubhabrataBarik <subhabratabarik7@gmail.com>
2026-03-03 00:56:05 +0530

    AWS Deployment

--- frontend-login/js/config.js
+++ frontend-login/js/config.js
@@ -1,6 +1,7 @@
 // Configuration for the API
 const CONFIG = {
   // API_BASE_URL: 'http://127.0.0.1:8000/api',
-  API_BASE_URL: "https://taskmaster-e0oy.onrender.com/api",
+  // API_BASE_URL: "https://taskmaster-e0oy.onrender.com/api",
+  API_BASE_URL: "http://3.7.233.37/api",
   GOOGLE_CLIENT_ID: '76063238802-mmeve6gl126ntmhl0b6b7987t2agvb3m.apps.googleusercontent.com'
 };
